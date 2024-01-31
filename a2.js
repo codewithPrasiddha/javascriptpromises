acde54ba8cdf4983a3c6ceac41189c3776f8a540
Prasiddha Thapaliya <th.prasiddha@gmail.com>
2024-01-30 23:44:07 -0500

    Declaration

---
+++ a2.js
@@ -0,0 +1,9 @@
+/*********************************************************************************
+*  WEB700 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Prasiddha Thapaliya Student ID:121569230 Date: 30th January, 2024
+*
+********************************************************************************/ 
