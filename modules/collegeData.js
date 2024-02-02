6a02bc60c596b4a26638799e2a73c6d67e5f69b0
Prasiddha Thapaliya <th.prasiddha@gmail.com>
2024-02-01 19:19:05 -0500

    Inititalize function using promise and importing data from json files

---
+++ modules/collegeData.js
@@ -0,0 +1,57 @@
+const courses = require("../data/courses.json");
+const students = require("../data/students.json");
+
+class Data{
+    constructor(students, courses){
+        this.students = students;
+        this.courses = courses;
+    }
+}
+
+let dataCollection = null;
+
+function initialize(){
+    dataCollection = new Data(students, courses);
+    return Promise.resolve('*****COLLEGE DATA HAS BEEN INITIALIZED*****');
+}
+
+function getAllStudents(){
+    return new Promise((resolve, reject)=>{ 
+        if (dataCollection.students === 0){
+            reject("no results returned");
+        }
+        else{
+            resolve(dataCollection.students);
+        }
+    });
+}
+
+function getTAs() {
+    return new Promise((resolve, reject) => {
+      const tas = dataCollection.students.filter(student => student.TA);
+      if (tas.length === 0) {
+        reject("no results returned.");
+      } 
+      else {
+        resolve(tas);
+      }
+    });
+  }
+
+  function getCourses() {
+    return new Promise((resolve, reject) => {
+      if (dataCollection.courses === 0) {
+        reject("no results returned.");
+      } 
+      else {
+        resolve(dataCollection.courses);
+      }
+    });
+  }
+
+  module.exports = {
+    initialize,
+    getAllStudents,
+    getTAs,
+    getCourses
+  };
